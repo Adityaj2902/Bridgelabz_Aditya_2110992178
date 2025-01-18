e9e8bac9222c8eafb259d97c103805720b6eecc1
Adityaj2902 <adityajofficial909@gmail.com>
2025-01-18 19:24:52 +0530

    Done With Practice

---
+++ mapmethod.ts/main.js
@@ -0,0 +1,3 @@
+var arr = [2, 4, 6, 8, 9];
+var ans = arr.map(function (curr, i) { return curr = curr * 2; });
+console.log(ans);
---
+++ mapmethod.ts/main.ts
@@ -0,0 +1,7 @@
+let arr:number []=[2,4,6,8,9];
+
+
+let  ans=arr.map((curr,i)=>curr=curr*2);
+
+
+console.log(ans);
