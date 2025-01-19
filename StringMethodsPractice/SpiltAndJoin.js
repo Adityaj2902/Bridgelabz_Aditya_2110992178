857bf5c64c2e4addfefb11a18feaff5f950f8e83
Adityaj2902 <adityajofficial909@gmail.com>
2025-01-20 00:15:13 +0530

    Done with the String methods

--- StringMethodsPractice/SpiltAndJoin.js
+++ StringMethodsPractice/SpiltAndJoin.js
@@ -1,10 +1,23 @@
 
 // SPLIT METHOD CONVERT THE STRING INTO A NEW ARRAY
 
-console.log('a+very+nice+string'.split('+'));
+// console.log('a+very+nice+string'.split('+'));
 
 // JOIN METHOD HELP US TO CONVERT THE ARRAY INTO A NEW STRING
 
-const [firstName,lastName]='Aditya Jaiswal'.split(' ');
+// const [firstName,lastName]='Aditya Jaiswal'.split(' ');
 
-console.log(firstName,lastName);
+// console.log(firstName,lastName);
+
+
+const capitalizeName=function(name){
+    const names=name.split(' ');
+    const namesUpper=[];
+
+    for(const n of names){
+       namesUpper.push(n[0].toUpperCase()+n.slice(1));
+    }
+    console.log(namesUpper.join(' '));
+}
+
+capitalizeName('aditya and vivek are good friends');
